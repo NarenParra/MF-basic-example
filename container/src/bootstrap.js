96b8d17e320231773e18bd6748b5b81fb820a8db
Naren Felipe Parra Trujillo - Ceiba Software <naren.parra@ceiba.com.co>
2021-06-21 11:45:08 -0500

    mount function

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,3 +1,4 @@
-import "products/ProductsIndex";
+import { mount } from "products/ProductsIndex";
 import "cart/CartIndex";
 console.log("container");
+mount(document.querySelector("#my-products"));
